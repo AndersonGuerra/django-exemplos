c57ddc78f86c48f96ec0d64cc8a84746f45feab5
Anderson <and.guerra@outlook.com>
2022-11-10 20:07:31 -0300

    Exemplos

---
+++ exemplos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def exemplo1(request):
+    return render(request, "exemplo1.html")
+
+def exemplo2(request):
+    return render(request, "exemplo2.html")
+
+def exemplo3(request):
+    return render(request, "exemplo3.html")
